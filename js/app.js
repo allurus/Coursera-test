d7492bd32043f150ee09573e43bae1994d204963
small <small@gmail.com>
2017-01-05 15:30:54 -0500

    Changes to test applicatons

---
+++ js/app.js
@@ -0,0 +1,27 @@
+(function () {
+  'use strict';
+
+  angular.module('myFirstApp',[])
+
+  .controller('myFirstController', function($scope){
+    $scope.name="";
+    $scope.totalValue=0;
+    $scope.sayHello = function(){
+      return "Hello Coursera!";
+    };
+    $scope.displayNumeric = function(){
+      var totalNameValue=calculateNumericForString($scope.name);
+      $scope.totalValue= totalNameValue;
+    };
+
+    function calculateNumericForString(string){
+      var totalStringValue=0;
+      for (var i=0; i<string.length; i++){
+        totalStringValue += string.charCodeAt(i);
+      }
+      return totalStringValue;
+    };
+
+  });
+
+})();
